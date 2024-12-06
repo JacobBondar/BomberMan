1664cbf567d230af47fb83f2f6e4c339311b96a9
Jacob Bondar <jacob.bondar@gmail.com>
2024-12-06 12:00:20 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -9,37 +9,5 @@ int main()
     Controller c;
 	c.run();
 
-    /*while (1)
-    {
-        int direction = _getch();
-
-        std::cout << "signal = " << direction << std::endl;
-        direction = _getch();
-        std::cout << "actual = " << direction << std::endl;
-    }*/
-    
-    
-    /*ifstream file, fileLevel;
-    string nameLevel;
-
-    file.open("Playlist.txt");
-    if (!file)
-    {
-        std::cerr << "Can't open the main file\n";
-        exit(EXIT_FAILURE);
-    }
-
-
-    while (file >> nameLevel)
-    {
-        cout << nameLevel << endl;
-        fileLevel.open(nameLevel);
-        if (!fileLevel)
-        {
-            std::cerr << "Can't open the game file, moving to the next one...\n"; // add timer pls ty
-            continue;
-        }
-    }
-    */
 	exit(EXIT_SUCCESS);
 }
